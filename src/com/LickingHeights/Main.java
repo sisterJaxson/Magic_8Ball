e3b957b2234bcfc2084a3b938683d6ee01236cb1
Unknown <jaxson.weaver.2022@student.lhschools.org>
2019-02-25 14:07:17 -0500

    Declares

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,10 +1,21 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hi");
-	System.out.println("hello");
-	
+	//System.out.println("Hi");
+	//System.out.println("hello");
+
+        Scanner keyboard;
+        int value;
+        int testing;
+        String question;
+        
+
+
+
+
     }
 }
