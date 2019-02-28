53967e5e56bc94292a7ab4584a9bc1aa92dfbe79
Unknown <jaxson.weaver.2022@student.lhschools.org>
2019-02-28 13:38:12 -0500

    Finished the rest of the else if statements

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -25,12 +25,26 @@ public class Main {
         else if(value>=20&&value<30){
             System.out.println("Defiantly");
         }
-
-
-
-
-
-
-
+        else if(value>=30&&value<40){
+            System.out.println("Maybe");
+        }
+        else if(value>=40&&value<50){
+            System.out.println("Bad question!");
+        }
+        else if(value>=50&&value<60){
+            System.out.println("DIE!!!!!");
+        }
+        else if(value>=60&&value<70){
+            System.out.println("Sure");
+        }
+        else if(value>=70&&value<80){
+            System.out.println("My reply is no!");
+        }
+        else if(value>=80&&value<90){
+            System.out.println("With time");
+        }
+        else if(value>=90&&value<=100){
+            System.out.println("Don't count on it!");
+        }
     }
 }
