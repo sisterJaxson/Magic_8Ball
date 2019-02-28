722b88e0ac1b309afccb5619dd1c6f9ae12db59a
Unknown <jaxson.weaver.2022@student.lhschools.org>
2019-02-28 13:28:50 -0500

    made multiple if and else if statements

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -9,16 +9,25 @@ public class Main {
 	//System.out.println("hello");
 
         Scanner keyboard;
+        int value;
         int testing;
         String question;
         keyboard= new Scanner(System.in);
 
         System.out.println("Pick a number 0 to 100 ");
-
-
-        if(0>10){
-
+        value = keyboard.nextInt();
+        if(value<10){
+            System.out.println("Of course!");
         }
+        else if(value>=10&&value<20){
+            System.out.println("Ask again");
+        }
+        else if(value>=20&&value<30){
+            System.out.println("Defiantly");
+        }
+
+
+
 
 
 
